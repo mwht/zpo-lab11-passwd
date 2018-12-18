252305cfb3a229f604cb9377a5fccb0e7440e741
Sebastian Madejski <madejski.s97@gmail.com>
2018-12-18 09:51:48 +0100

    fix regex + IntelliJ warnings

--- src/Password.scala
+++ src/Password.scala
@@ -1,5 +1,5 @@
 object Password {
-  var predicates:List[(String => Boolean)] = List()
+  var predicates:List[String => Boolean] = List()
 
   def validate(password: String)(min_len: Int)(max_len: Int): Boolean = {
     if(password.length < min_len || password.length > max_len) false
@@ -8,7 +8,7 @@ object Password {
     result
   }
 
-  def addPredicate(predicate: (String => Boolean)): Unit = predicates = predicates :+ predicate
+  def addPredicate(predicate: String => Boolean): Unit = predicates = predicates :+ predicate
 
   def upperCase(strToValidate: String): Boolean = strToValidate.matches(".*[A-Z].*")
 
@@ -16,6 +16,6 @@ object Password {
 
   def oneDigit(strToValidate: String): Boolean = strToValidate.matches(".*\\d.*")
 
-  def twoDigits(strToValidate: String): Boolean = strToValidate.matches(".*\\d.*?\\d.*")
+  def twoDigits(strToValidate: String): Boolean = strToValidate.matches(".*\\d.*\\d.*")
 
 }
